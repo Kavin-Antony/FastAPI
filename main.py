8f59105b92de286d52b745e0aa330f9614db26ec
Kavin-Antony <kavinantony678@gmail.com>
2025-02-19 23:34:55 +0530

    day1

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def index():
 @app.get('/about')
 def about():
     return {'data':{'about page'}}
+
+@app.get('/blog/{id}')
+def blog(id):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    data = {1:20, 100:30}
+    return data[id]
